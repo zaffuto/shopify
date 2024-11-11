ad0920243d620bf21a77a9567a91595f9f21e72e
SmarterBOT <hola@smarterbot.cl>
2024-11-11 19:21:07 -0300

    V2.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://smarterbot.cl/signup"
+            href="https://www.smarterbot.cl/subscribe"
             target="_blank"
             rel="noopener noreferrer"
           >
